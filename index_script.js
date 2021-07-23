85150f6a725d76215bc14902aa4c4861121b6767
N-eeraj <neeraj7rajeena@gmail.com>
2021-07-23 20:38:16 +0530

    added responsiveness to index info

---
+++ index_script.js
@@ -0,0 +1,4 @@
+const openIndexInfo = () => document.getElementById("index-info").style.transform = "scale(1)";
+const closeIndexInfo = () => document.getElementById("index-info").style.transform = "scale(0)";
+
+openIndexInfo()
